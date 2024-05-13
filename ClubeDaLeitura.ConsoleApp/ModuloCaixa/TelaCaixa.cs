28be2f862a790ac1a368f379d13fb26cdd294c09
Tiago Santini <tiagosantiniathayde@outlook.com>
2024-05-13 10:32:23 -0300

    Implementado revistas

--- ClubeDaLeitura.ConsoleApp/ModuloCaixa/TelaCaixa.cs
+++ ClubeDaLeitura.ConsoleApp/ModuloCaixa/TelaCaixa.cs
@@ -1,4 +1,5 @@
 ﻿using ControleMedicamentos.ConsoleApp.Compartilhado;
+using System.Collections;
 
 namespace ClubeDaLeitura.ConsoleApp.ModuloCaixa
 {
@@ -6,12 +7,48 @@ namespace ClubeDaLeitura.ConsoleApp.ModuloCaixa
     {
         public override void VisualizarRegistros(bool exibirTitulo)
         {
-            throw new NotImplementedException();
+            if (exibirTitulo)
+            {
+                ApresentarCabecalho();
+
+                Console.WriteLine("Visualizando Caixas...");
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine(
+                "{0, -10} | {1, -20} | {2, -20} | {3, -10}",
+                "Id", "Etiqueta", "Cor", "Tempo de Empréstimo"
+            );
+
+            ArrayList caixasCadastradas = repositorio.SelecionarTodos();
+
+            foreach (Caixa caixa in caixasCadastradas)
+            {
+                if (caixa == null)
+                    continue;
+
+                Console.WriteLine(
+                    "{0, -10} | {1, -20} | {2, -20} | {3, -10}",
+                    caixa.Id, caixa.Etiqueta, caixa.Cor, caixa.TempoEmprestimo
+                );
+            }
+
+            Console.WriteLine();
         }
 
         protected override EntidadeBase ObterRegistro()
         {
-            throw new NotImplementedException();
+            Console.Write("Digite a etiqueta da caixa: ");
+            string etiqueta = Console.ReadLine();
+
+            Console.Write("Digite a cor da caixa: ");
+            string cor = Console.ReadLine();
+
+            Console.Write("Digite o tempo de empréstimo máximo: ");
+            int tempoEmprestimo = Convert.ToInt32(Console.ReadLine());
+
+            return new Caixa(etiqueta, cor, tempoEmprestimo);
         }
     }
 }
---
+++ ClubeDaLeitura.ConsoleApp/ModuloRevista/TelaRevista.cs
@@ -0,0 +1,65 @@
+﻿using ClubeDaLeitura.ConsoleApp.ModuloCaixa;
+using ControleMedicamentos.ConsoleApp.Compartilhado;
+using System.Collections;
+
+namespace ClubeDaLeitura.ConsoleApp.ModuloRevista
+{
+    public class TelaRevista : TelaBase
+    {
+        public TelaCaixa telaCaixa = null;
+        public RepositorioCaixa repositorioCaixa = null;
+
+        public override void VisualizarRegistros(bool exibirTitulo)
+        {
+            if (exibirTitulo)
+            {
+                ApresentarCabecalho();
+
+                Console.WriteLine("Visualizando Revistas...");
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine(
+                "{0, -10} | {1, -20} | {2, -20} | {3, -10} | {4, -20}",
+                "Id", "Titulo", "Edição", "Ano", "Caixa"
+            );
+
+            ArrayList revistasCadastradas = repositorio.SelecionarTodos();
+
+            foreach (Revista revista in revistasCadastradas)
+            {
+                if (revista == null)
+                    continue;
+
+                Console.WriteLine(
+                    "{0, -10} | {1, -20} | {2, -20} | {3, -10} | {4, -20}",
+                    revista.Id, revista.Titulo, revista.NumeroEdicao, revista.Ano, revista.Caixa.Etiqueta
+                );
+            }
+
+            Console.WriteLine();
+        }
+
+        protected override EntidadeBase ObterRegistro()
+        {
+            Console.Write("Digite o título da revista: ");
+            string titulo = Console.ReadLine();
+
+            Console.Write("Digite o número da edição: ");
+            int numeroEdicao = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o ano de lançamento: ");
+            int ano = Convert.ToInt32(Console.ReadLine());
+
+            telaCaixa.VisualizarRegistros(false);
+
+            Console.Write("Digite o ID da caixa em que deseja guardar a revista: ");
+            int idCaixa = Convert.ToInt32(Console.ReadLine());
+
+            Caixa caixaSelecionada = (Caixa)repositorioCaixa.SelecionarPorId(idCaixa);
+
+            return new Revista(titulo, numeroEdicao, ano, caixaSelecionada);
+        }
+    }
+}
