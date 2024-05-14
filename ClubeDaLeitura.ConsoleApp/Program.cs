ca240297333ecee00a4d7336fe76d1635d699e93
Tiago Santini <tiagosantiniathayde@outlook.com>
2024-05-14 12:55:22 -0300

    Adicionadas dicas

--- ClubeDaLeitura.ConsoleApp/Program.cs
+++ ClubeDaLeitura.ConsoleApp/Program.cs
@@ -3,6 +3,27 @@ using ClubeDaLeitura.ConsoleApp.ModuloAmigo;
 
 namespace ClubeDaLeitura.ConsoleApp
 {
+    #region Interfaces
+    //  Interfaces definem um contrato que todas as classes que a implementam deve seguir
+
+    //      Uma interface declara "o que uma classe deve ter"
+    //      Uma classe implementando a interface define "como deve ser feito"
+
+    //      Benefícios = herança múltipla + extensibilidade do código
+
+    //      Geralmente, interfaces são declaradas com o prefixo I, ex: ITelaCadastravel
+    #endregion
+
+    #region Generics
+    //  Generics permitem criarmos código não-específico para tipos de dados abrangentes
+
+    //      É possível adicionar o marcador <T> para: classes, métodos, campos, etc.
+
+    //      Benefícios = reusabilidade de código para tipos de dados específicos
+    //          + identificação de código genérico em tempo de desenvolvimento
+    //          + diminuição o uso de cast (Tipo)
+    #endregion
+
     class Program
     {
         static void Main(string[] args)
